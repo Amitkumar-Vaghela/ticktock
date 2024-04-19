dd11cf1a8bd9d0374d1470759df74d360a2d8127
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2024-04-19 18:02:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let count = 10;
-    const timerDisplay = document.getElementById('timer');
 
-    function countdown() {
-        timerDisplay.textContent = count;
-        count--;
-
-        if (count < 0) {
-            count = 10; // Reset the countdown
-        }
-    }
-
-    setInterval(countdown, 1000);
-});
